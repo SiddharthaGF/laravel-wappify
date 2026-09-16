1b3185abfddabb1e204f7bd5303010ce02d402c2
Pablo Santiago Guerra <psgf.tech+github@gmail.com>
2026-09-16 04:06:49 -0500

    refactor: remove global whatsapp() helper behind v2 facade

--- src/helpers.php
+++ src/helpers.php
@@ -3,24 +3,7 @@
 declare(strict_types=1);
 
 use AiluraCode\Wappify\Actions\VerifyWebhookChallenge;
-use AiluraCode\Wappify\Data\WhatsappAccountConfig;
-use AiluraCode\Wappify\WhatsAppCloudApi;
 use Illuminate\Http\Request;
-
-/**
- * @deprecated Will be removed in v2.0. Resolve the transport via the account config instead.
- */
-function whatsapp(string $account = 'default'): WhatsAppCloudApi
-{
-    trigger_error('whatsapp() is deprecated and will be removed in v2.0.', E_USER_DEPRECATED);
-
-    $config = WhatsappAccountConfig::fromConfig($account);
-
-    return new WhatsAppCloudApi([
-        'from_phone_number_id' => $config->number_id,
-        'access_token' => $config->token,
-    ]);
-}
 
 /**
  * @deprecated Will be removed in v2.0. Use `AiluraCode\Wappify\Actions\VerifyWebhookChallenge` instead.
